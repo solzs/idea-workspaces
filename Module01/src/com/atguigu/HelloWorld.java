846739202953f941aee95573a2f5789377944ee3
unknown <zhoushen@bilibili.local>
2019-06-08 18:35:03 +0800

    Initial commit

---
+++ Module01/src/com/atguigu/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.atguigu;
+
+public class HelloWorld {
+//    system.out.println("hello!!");
+public static void main(String[] args) {
+    System.out.println("hello!!");
+    System.out.println();
+    String use = new String("use");
+}
+}
---
+++ Module01/src/com/atguigu/Test1.java
@@ -0,0 +1,8 @@
+package com.atguigu;
+
+/**
+ * @author zs
+ * @date 2019/6/7 - 14:28
+ */
+public class Test1 {
+}
